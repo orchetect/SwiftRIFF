f2f4308a79f56a9009c9b7c6d6f57f44f4eee2c4
Steffan Andrews <stef@orchetect.com>
2025-06-17 15:39:18 -0700

    Unit tests cleanup

--- Tests/SwiftRIFFTests/SwiftRIFFTests.swift
+++ Tests/SwiftRIFFTests/SwiftRIFFTests.swift
@@ -2,45 +2,42 @@ import Foundation
 @testable import SwiftRIFF
 import Testing
 
-// (all integers are stored little-endian)
-// note that this mocks the structure of a wave file for purposes of unit testing,
-// but does not actually contain a valid wave file data chunk.
-// it is however correctly formatted as a RIFF file.
-private let riffBytes: [UInt8] = [
-    // start of file
-    0x52, 0x49, 0x46, 0x46, // "RIFF"
-    0x28, 0x00, 0x00, 0x00, // Total file length minus 8 bytes == int 40
-    0x57, 0x41, 0x56, 0x45, // "WAVE" file type
-    0x66, 0x6D, 0x74, 0x20, // “fmt "
-    0x10, 0x00, 0x00, 0x00, // Format chunk length == int 16
-    0x01, 0x00, // Format type. PCM == int 1
-    0x02, 0x00, // Number of channels == int 2
-    0x80, 0xBB, 0x00, 0x00, // Sample Rate == int 48_000
-    0x00, 0x65, 0x04, 0x00, // (SampleRate * BitsPerSample * Channels) / 8 == int 288_000
-    0x06, 0x00, // (BitsPerSample * Channels) / 8 == int 6
-    0x18, 0x00, // Bits per sample == int 24
-    0x64, 0x61, 0x74, 0x61, // "data" chunk ID
-    0x03, 0x00, 0x00, 0x00, // Data chunk length == int 3
-    0x01, 0x02, 0x03, 0x00 // 3 bytes of data + 1 byte of null padding
-]
-
-private let fmtBytes: [UInt8] = [
-    0x66, 0x6D, 0x74, 0x20, // “fmt "
-    0x10, 0x00, 0x00, 0x00, // Format chunk length == int 16
-    0x01, 0x00, // Format type. PCM == int 1
-    0x02, 0x00, // Number of channels == int 2
-    0x80, 0xBB, 0x00, 0x00, // Sample Rate == int 48_000
-    0x00, 0x65, 0x04, 0x00, // (SampleRate * BitsPerSample * Channels) / 8 == int 288_000
-    0x06, 0x00, // (BitsPerSample * Channels) / 8 == int 6
-    0x18, 0x00 // Bits per sample == int 24
-]
+private enum SampleRIFF {
+    // (all integers are stored little-endian)
+    // note that this mocks the structure of a wave file for purposes of unit testing,
+    // but does not actually contain a valid wave file data chunk.
+    // it is however correctly formatted as a RIFF file.
+    static let fileBytes: [UInt8] = [
+        // start of file
+        0x52, 0x49, 0x46, 0x46, // "RIFF"
+        0x28, 0x00, 0x00, 0x00, // Total file length minus 8 bytes == int 40
+        0x57, 0x41, 0x56, 0x45 // "WAVE" file type
+    ] + fmtChunkBytes + dataChunkBytes
+    
+    static let fmtChunkBytes: [UInt8] = [
+        0x66, 0x6D, 0x74, 0x20, // “fmt "
+        0x10, 0x00, 0x00, 0x00, // Format chunk length == int 16
+        0x01, 0x00, // Format type. PCM == int 1
+        0x02, 0x00, // Number of channels == int 2
+        0x80, 0xBB, 0x00, 0x00, // Sample Rate == int 48_000
+        0x00, 0x65, 0x04, 0x00, // (SampleRate * BitsPerSample * Channels) / 8 == int 288_000
+        0x06, 0x00, // (BitsPerSample * Channels) / 8 == int 6
+        0x18, 0x00 // Bits per sample == int 24
+    ]
+    
+    static let dataChunkBytes: [UInt8] = [
+        0x64, 0x61, 0x74, 0x61, // "data" chunk ID
+        0x03, 0x00, 0x00, 0x00, // Data chunk length == int 3
+        0x01, 0x02, 0x03, 0x00 // 3 bytes of data + 1 byte of null padding
+    ]
+}
 
 @Test
 func parseRIFFDescriptor() async throws {
     // write to file on disk so we can parse it
     let tempFile = URL.temporaryDirectory.appending(component: "\(UUID().uuidString).riff")
     print("Writing to temp file: \(tempFile.path)")
-    try Data(riffBytes).write(to: tempFile)
+    try Data(SampleRIFF.fileBytes).write(to: tempFile)
     
     let h = try FileHandle(forReadingFrom: tempFile)
     
@@ -59,7 +56,7 @@ func parseFMTChunkDescriptor() async throws {
     // write to file on disk so we can parse it
     let tempFile = URL.temporaryDirectory.appending(component: "\(UUID().uuidString).riff")
     print("Writing to temp file: \(tempFile.path)")
-    try Data(fmtBytes).write(to: tempFile)
+    try Data(SampleRIFF.fmtChunkBytes).write(to: tempFile)
     
     let h = try FileHandle(forReadingFrom: tempFile)
     
@@ -78,7 +75,7 @@ func parseRIFFFile() async throws {
     // write to file on disk so we can parse it
     let tempFile = URL.temporaryDirectory.appending(component: "\(UUID().uuidString).riff")
     print("Writing to temp file: \(tempFile.path)")
-    try Data(riffBytes).write(to: tempFile)
+    try Data(SampleRIFF.fileBytes).write(to: tempFile)
     
     let riffFile = try RIFFFile(url: tempFile)
     
@@ -116,7 +113,7 @@ func writeRIFFFileChunk() async throws {
     // write to file on disk so we can parse it
     let tempFile = URL.temporaryDirectory.appending(component: "\(UUID().uuidString).riff")
     print("Writing to temp file: \(tempFile.path)")
-    try Data(riffBytes).write(to: tempFile)
+    try Data(SampleRIFF.fileBytes).write(to: tempFile)
     
     // read existing chunk
     var riffFile = try RIFFFile(url: tempFile)
