8bfc35d24202511594542b59ff9c909241b33774
Steffan Andrews <stef@orchetect.com>
2025-07-17 00:16:39 -0700

    Added `Data` `nullTerminatedASCIIString` utility method

---
+++ Sources/SwiftRIFFCore/RIFFFile/Utilities/Data Utilities.swift
@@ -0,0 +1,32 @@
+//
+//  Data Utilities.swift
+//  SwiftRIFF • https://github.com/orchetect/SwiftRIFF
+//  © 2025-2025 Steffan Andrews • Licensed under MIT License
+//
+
+import Foundation
+import OTCore
+
+extension Data {
+    /// Returns an ASCII string encoded as null-terminated.
+    /// If all bytes are non-null, it is assumed no null character is required and all bytes are ASCII characters.
+    package func nullTerminatedASCIIString(in range: ClosedRange<Index>) -> String? {
+        let firstNullIndex = firstRange(of: [0x00 as UInt8], in: range)?.lowerBound
+        
+        if firstNullIndex == nil {
+            // possibly all bytes are occupied (non-null)
+            return self[range].toString(using: .ascii)
+        }
+        
+        // Check for empty string (first char is a null byte)
+        if firstNullIndex == range.lowerBound {
+            return ""
+        }
+        
+        // otherwise, assume all bytes prior to the null are ASCII characters
+        guard let firstNullIndex else { return nil }
+        let parseRange = range.lowerBound ..< firstNullIndex
+        return data[parseRange]
+            .toString(using: .ascii)
+    }
+}
---
+++ Tests/SwiftRIFFCoreTests/Utilities/Data Utilities Tests.swift
@@ -0,0 +1,55 @@
+//
+//  Data Utilities Tests.swift
+//  SwiftRIFF • https://github.com/orchetect/SwiftRIFF
+//  © 2025-2025 Steffan Andrews • Licensed under MIT License
+//
+
+import Foundation
+@testable import SwiftRIFFCore
+import Testing
+
+@Suite struct DataUtilitiesTests {
+    @Test
+    func nullTerminatedASCIIString() throws {
+        // #expect(Data().nullTerminatedASCIIString(in: 0 ... 0) == nil) // would crash (as expected)
+        #expect(Data([0x00]).nullTerminatedASCIIString(in: 0 ... 0) == "")
+        
+        #expect(Data([0x00, 0x00]).nullTerminatedASCIIString(in: 0 ... 0) == "")
+        #expect(Data([0x00, 0x00]).nullTerminatedASCIIString(in: 0 ... 1) == "")
+        #expect(Data([0x00, 0x00, 0x00]).nullTerminatedASCIIString(in: 0 ... 0) == "")
+        #expect(Data([0x00, 0x00, 0x00]).nullTerminatedASCIIString(in: 0 ... 1) == "")
+        #expect(Data([0x00, 0x00, 0x00]).nullTerminatedASCIIString(in: 0 ... 2) == "")
+        
+        #expect(Data([0x31]).nullTerminatedASCIIString(in: 0 ... 0) == "1")
+        #expect(Data([0x31, 0x00]).nullTerminatedASCIIString(in: 0 ... 0) == "1")
+        #expect(Data([0x31, 0x00]).nullTerminatedASCIIString(in: 0 ... 1) == "1")
+        #expect(Data([0x31, 0x00, 0x00]).nullTerminatedASCIIString(in: 0 ... 0) == "1")
+        #expect(Data([0x31, 0x00, 0x00]).nullTerminatedASCIIString(in: 0 ... 1) == "1")
+        #expect(Data([0x31, 0x00, 0x00]).nullTerminatedASCIIString(in: 0 ... 2) == "1")
+        
+        #expect(Data([0x31, 0x00, 0x32]).nullTerminatedASCIIString(in: 0 ... 0) == "1")
+        #expect(Data([0x31, 0x00, 0x32]).nullTerminatedASCIIString(in: 0 ... 1) == "1")
+        #expect(Data([0x31, 0x00, 0x32]).nullTerminatedASCIIString(in: 0 ... 2) == "1")
+        #expect(Data([0x31, 0x00, 0x32]).nullTerminatedASCIIString(in: 1 ... 1) == "")
+        #expect(Data([0x31, 0x00, 0x32]).nullTerminatedASCIIString(in: 1 ... 2) == "")
+        #expect(Data([0x31, 0x00, 0x32]).nullTerminatedASCIIString(in: 2 ... 2) == "2")
+        
+        #expect(Data([0x31, 0x00, 0x32, 0x33]).nullTerminatedASCIIString(in: 2 ... 2) == "2")
+        #expect(Data([0x31, 0x00, 0x32, 0x33]).nullTerminatedASCIIString(in: 2 ... 3) == "23")
+        #expect(Data([0x31, 0x00, 0x32, 0x33, 0x00]).nullTerminatedASCIIString(in: 2 ... 2) == "2")
+        #expect(Data([0x31, 0x00, 0x32, 0x33, 0x00]).nullTerminatedASCIIString(in: 2 ... 3) == "23")
+        #expect(Data([0x31, 0x00, 0x32, 0x33, 0x00]).nullTerminatedASCIIString(in: 2 ... 4) == "23")
+        #expect(Data([0x31, 0x00, 0x32, 0x33, 0x00]).nullTerminatedASCIIString(in: 3 ... 4) == "3")
+        #expect(Data([0x31, 0x00, 0x32, 0x33, 0x00]).nullTerminatedASCIIString(in: 4 ... 4) == "")
+        
+        #expect(Data([0x00, 0x31, 0x00, 0x32, 0x33]).nullTerminatedASCIIString(in: 3 ... 3) == "2")
+        #expect(Data([0x00, 0x31, 0x00, 0x32, 0x33]).nullTerminatedASCIIString(in: 3 ... 4) == "23")
+        #expect(Data([0x00, 0x31, 0x00, 0x32, 0x33]).nullTerminatedASCIIString(in: 0 ... 4) == "")
+        #expect(Data([0x00, 0x31, 0x00, 0x32, 0x33, 0x00]).nullTerminatedASCIIString(in: 3 ... 3) == "2")
+        #expect(Data([0x00, 0x31, 0x00, 0x32, 0x33, 0x00]).nullTerminatedASCIIString(in: 3 ... 4) == "23")
+        #expect(Data([0x00, 0x31, 0x00, 0x32, 0x33, 0x00]).nullTerminatedASCIIString(in: 3 ... 5) == "23")
+        #expect(Data([0x00, 0x31, 0x00, 0x32, 0x33, 0x00]).nullTerminatedASCIIString(in: 4 ... 5) == "3")
+        #expect(Data([0x00, 0x31, 0x00, 0x32, 0x33, 0x00]).nullTerminatedASCIIString(in: 5 ... 5) == "")
+        #expect(Data([0x00, 0x31, 0x00, 0x32, 0x33, 0x00]).nullTerminatedASCIIString(in: 0 ... 5) == "")
+    }
+}
