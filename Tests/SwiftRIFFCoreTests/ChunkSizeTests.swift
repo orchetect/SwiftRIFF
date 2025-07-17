b579f3f9272baf2cf7afde2d1c8565ebd85f98e7
Steffan Andrews <stef@orchetect.com>
2025-07-16 17:11:59 -0700

    Updated unit tests

--- Tests/SwiftRIFFCoreTests/ChunkSizeTests.swift
+++ Tests/SwiftRIFFCoreTests/ChunkSizeTests.swift
@@ -23,33 +23,33 @@ private enum SampleRIFF {
     + chk4ChunkBytes
     
     static let chk0ChunkBytes: [UInt8] = [
-        0x63, 0x68, 0x6B, 0x31, // “chk1"
+        0x63, 0x68, 0x6B, 0x30, // “chk0"
         0x00, 0x00, 0x00, 0x00 // Chunk length == int 0
         // no data bytes
     ]
     
     static let chk1ChunkBytes: [UInt8] = [
-        0x63, 0x68, 0x6B, 0x32, // “chk2"
+        0x63, 0x68, 0x6B, 0x31, // “chk1"
         0x01, 0x00, 0x00, 0x00, // Chunk length == int 1
         0x7F, // one data byte
         0x00 // one null data pad byte
     ]
     
     static let chk2ChunkBytes: [UInt8] = [
-        0x63, 0x68, 0x6B, 0x33, // “chk3"
+        0x63, 0x68, 0x6B, 0x32, // “chk2"
         0x02, 0x00, 0x00, 0x00, // Chunk length == int 2
         0x40, 0x41 // two data bytes
     ]
     
     static let chk3ChunkBytes: [UInt8] = [
-        0x63, 0x68, 0x6B, 0x34, // “chk4"
+        0x63, 0x68, 0x6B, 0x33, // “chk3"
         0x03, 0x00, 0x00, 0x00, // Chunk length == int 3
         0x40, 0x41, 0x42, // three data bytes
         0x00 // one null data pad byte
     ]
     
     static let chk4ChunkBytes: [UInt8] = [
-        0x63, 0x68, 0x6B, 0x35, // “chk5"
+        0x63, 0x68, 0x6B, 0x34, // “chk4"
         0x04, 0x00, 0x00, 0x00, // Chunk length == int 4
         0x40, 0x41, 0x42, 0x43 // four data bytes
     ]
@@ -87,6 +87,11 @@ private enum SampleRIFF {
         let riffFile = try RIFFFile(url: tempFile)
         
         #expect(riffFile.riffFormat == .riff)
-        #expect(riffFile.chunks.count == 5)
+        #expect(riffFile.chunks.count == 1)
+        
+        let mainChunk = riffFile.chunks[0]
+        #expect(mainChunk.id == .riff)
+        
+        #expect(mainChunk.chunks?.count == 5)
     }
 }
